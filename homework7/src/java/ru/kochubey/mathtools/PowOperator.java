ad5a6128eef7d3d121e91733e623076921e099c7
michaelkochubey <mikocska88@gmail.com>
2024-07-06 19:51:12 +0300

    [MPTI-7]: исправление 4.1.4

--- homework7/src/java/ru/kochubey/mathtools/PowOperator.java
+++ homework7/src/java/ru/kochubey/mathtools/PowOperator.java
@@ -1,8 +1,11 @@
 package ru.kochubey.mathtools;
 
+import static java.lang.Math.pow;
+import static java.lang.Integer.parseInt;
+
 public class PowOperator {
     public static double raiseToPower(String x, String y) {
-        int x_ = Integer.parseInt(x), y_ = Integer.parseInt(y);
-        return Math.pow(x_, y_);
+        int x_ = parseInt(x), y_ = parseInt(y);
+        return pow(x_, y_);
     }
 }
